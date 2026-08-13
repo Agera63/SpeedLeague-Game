385235301690d983ad8cd4f649fd48be2c4b9dfc
DESKTOP-JNGH4DF\Eric <ericz2016@gmail.com>
2026-08-13 09:36:15 -0400

    fix: fixed bug preventing player from completing a lap

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -6,12 +6,6 @@ public class Checkpoint : MonoBehaviour
     public int checkpointNumber;
     public event Action<GameObject> onCheckpointEnter;
 
-    void Awake()
-    {
-        //This number will be overrided in TimeTrialMode class
-        checkpointNumber = 0;
-    }
-
     /// <summary>
     /// Once the a vehicle makes contact with a checkpoint, it gets registered.
     /// </summary>
