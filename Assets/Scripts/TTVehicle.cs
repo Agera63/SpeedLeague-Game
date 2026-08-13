3e13b6f85c8fb0b1a16fb4fca650bcaa7346eedb
DESKTOP-JNGH4DF\Eric <ericz2016@gmail.com>
2026-08-13 09:42:11 -0400

    fix: fixed checkpoint contact being called repetitively

--- Assets/Scripts/TTVehicle.cs
+++ Assets/Scripts/TTVehicle.cs
@@ -2,12 +2,17 @@ using UnityEngine;
 
 public class TTVehicule : MonoBehaviour
 {
+    private GameObject hitCheckpoint;
+
     void OnTriggerEnter(Collider other)
     {
         //Once a checkpoint has been hit, we register it
-        if (other.TryGetComponent(out Checkpoint cp))
+        if (other.TryGetComponent(out Checkpoint cp) && (hitCheckpoint == null || hitCheckpoint.GetComponent<Checkpoint>() != cp))
         {
+            //Stores the hit check point to prevent lag
+            hitCheckpoint = other.gameObject;
             cp.VehicleContact();
+
         }
     }
 }
